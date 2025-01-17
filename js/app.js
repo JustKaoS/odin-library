6b0cd19887559c799621373254e8811f3305cf53
Daniel Mott <daniel.r.mott@gmail.com>
2025-01-16 16:22:12 -0800

    added delete button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+const modal = document.getElementById("modal");
+const modalBackdrop = document.getElementById("modalBackdrop");
+const btn = document.getElementById("myBtn");
+let bookId = 0;
+
 const myLibrary = [
     {
         title: 'Choke',
@@ -14,16 +19,73 @@ const myLibrary = [
     }
 ];
 
-function Book(title, author, pages, hasRead) {
+displayBooks();
+
+function Book(title, author, pages, hasRead, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.hasRead = hasRead;
+    this.id = id;
 }
 
-function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
+function addBookToLibrary() {
+    const book = new Book();
+    let formTitle = document.getElementById("formTitle");
+    let formAuthor = document.getElementById("formAuthor");
+    let formPages = document.getElementById("formPages");
+    let formRead = document.getElementById("formRead");
+
+    book.title = formTitle.value;
+    book.author = formAuthor.value;
+    book.pages = parseInt(formPages.value);
+    book.hasRead = formRead.checked;
+    book.id = bookId;
+
+    const bookExists = myLibrary.some(existingBook =>
+        existingBook.title === book.title);
+
+    if (bookExists) {
+        alert("This book is already in the library!");
+    } else {
+        myLibrary.push(book);
+        console.log(myLibrary);
+    }
+    displayBooks();
 }
+
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    addBookToLibrary();
+    formTitle.value = "";
+    formAuthor.value = "";
+    formPages.value = "";
+    formRead.checked = false;
+    bookId++;
+    console.log(myLibrary);
+});
+
+const deleteBtn = document.getElementById("delete");
+deleteBtn.onclick = function () {
+    myLibrary.splice(this.id);
+    // myLibrary.some(title: )
+}
+
+btn.onclick = function () {
+    modal.style.display = "block";
+    modalBackdrop.style.display = "block";
+    modal.classList.remove("hidden");
+    modalBackdrop.classList.remove("hidden");
+}
+
+window.onclick = function (e) {
+    if (e.target == modal || e.target == modalBackdrop) {
+        modal.style.display = "none";
+        modalBackdrop.style.display = "none";
+    }
+}
+
 
 
 
@@ -31,7 +93,9 @@ function addBookToLibrary(newBook) {
 // Would like to add cover images accessing openlibrary api
 function displayBooks() {
     const container = document.querySelector(".container");
+    container.innerHTML = '';
     for (const book of myLibrary) {
+        
         const card = document.createElement("div");
         card.setAttribute("class", "card");
         container.appendChild(card);
@@ -40,25 +104,34 @@ function displayBooks() {
         cardHeader.setAttribute("class", "card-header");
         card.appendChild(cardHeader);
 
+        const titleDiv = document.createElement("div");
+        titleDiv.setAttribute("class", "titleDiv");
+        cardHeader.appendChild(titleDiv);
+
+        const bookDelete = document.createElement("span");
+        bookDelete.innerHTML = `<span id="delete">&times;</span>`
+        cardHeader.appendChild(bookDelete);
+ 
         const cardFooter = document.createElement("div");
         cardFooter.setAttribute("class", "card-footer");
         card.appendChild(cardFooter);
 
 
-        //Loops through each object in myLibrary and displays value of each key
+        //Loops through each object in myLibrary and displays value of each key to create
+        // the card layout
         for (const [key, value] of Object.entries(book)) {
 
             if (key === "title") {
                 const bookTitle = document.createElement("h2");
                 bookTitle.setAttribute("class", "title");
                 bookTitle.innerText = value;
-                cardHeader.appendChild(bookTitle);
+                titleDiv.appendChild(bookTitle);
 
             } else if (key === "author") {
                 const bookAuthor = document.createElement("h3");
                 bookAuthor.setAttribute("class", "author");
                 bookAuthor.innerText = value;
-                cardHeader.appendChild(bookAuthor);
+                titleDiv.appendChild(bookAuthor);
 
             } else if (key === "pages") {
                 const bookPages = document.createElement("p");
@@ -78,10 +151,5 @@ function displayBooks() {
 }
 
 
-displayBooks();
 
 
-// const warbreaker = new Book('Warbreaker', 'Sanderson', 592, 'Yes');
-// myLibrary.push(warbreaker);
-
-// console.log(myLibrary)
